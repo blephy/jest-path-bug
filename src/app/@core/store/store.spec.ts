455e58e399b6fdc195a9fbdb9111318236e2e1a5
Allan Dollé <hello@allandolle.fr>
2021-07-08 14:53:46 +0200

    test(coverage): make 100% stryker coverage

--- src/app/@core/store/store.spec.ts
+++ src/app/@core/store/store.spec.ts
@@ -8,10 +8,13 @@ describe("Square method", () => {
   it("should return right value", () => {
     const actual = square(1);
     const actual2 = square(3);
+    const actual3 = square();
     const expected = 1;
     const expected2 = 9;
+    const expected3 = 0;
 
     expect(actual).toStrictEqual(expected);
     expect(actual2).toStrictEqual(expected2);
+    expect(actual3).toStrictEqual(expected3);
   });
 });
